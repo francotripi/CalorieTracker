e66dbcb57157c9ed3903c4320faac24383addd46
Franco Tripi <francotripi92@gmail.com>
2022-10-11 21:53:55 +0200

    Implemented onboarding Nutrient Goal screen

--- app/src/main/java/com/francotripi/calorietracker/MainActivity.kt
+++ app/src/main/java/com/francotripi/calorietracker/MainActivity.kt
@@ -19,6 +19,7 @@ import com.francotripi.onboarding_presentation.age.AgeScreen
 import com.francotripi.onboarding_presentation.gender.GenderScreen
 import com.francotripi.onboarding_presentation.goal.GoalScreen
 import com.francotripi.onboarding_presentation.height.HeightScreen
+import com.francotripi.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.francotripi.onboarding_presentation.weight.WeightScreen
 import com.francotripi.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -64,7 +65,10 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable(Route.NUTRIENT_GOAL) {
-
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.ACTIVITY) {
                             ActivityScreen(onNavigate = navController::navigate)
