f44f542f40dd39c1edf189be45dd379855047d41
Franco Tripi <francotripi92@gmail.com>
2022-10-12 21:29:26 +0200

    Implemented Tracker Use Cases

---
+++ tracker/tracker_domain/src/main/java/com/francotripi/tracker_domain/use_case/DeleteTrackedFood.kt
@@ -0,0 +1,13 @@
+package com.francotripi.tracker_domain.use_case
+
+import com.francotripi.tracker_domain.model.TrackedFood
+import com.francotripi.tracker_domain.repository.TrackerRepository
+
+class DeleteTrackedFood(
+    private val repository: TrackerRepository
+) {
+
+    suspend operator fun invoke(trackedFood: TrackedFood) {
+        return repository.deleteTrackedFood(trackedFood)
+    }
+}
