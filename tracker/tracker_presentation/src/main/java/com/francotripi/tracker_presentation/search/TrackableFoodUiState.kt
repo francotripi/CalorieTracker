cc6dd280fcc2dea1ecb02d4e452ee00dd0e394f1
Franco Tripi <francotripi92@gmail.com>
2022-10-14 22:21:13 +0200

    Implemented SearchViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/francotripi/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.francotripi.tracker_presentation.search
+
+import com.francotripi.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
